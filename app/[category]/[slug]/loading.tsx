50789e140f9f3d5e495b31e8851fdbbc8e5a3a62
jhsonnn <jihyeongson13@gmail.com>
2025-03-19 12:21:06 +0900

    Fix: categoryFilter 타입오류 수정, Style: 스켈레톤 UI 수정

--- app/[category]/[slug]/loading.tsx
+++ app/[category]/[slug]/loading.tsx
@@ -4,6 +4,15 @@ import Skeleton from '@/components/ui/Skeleton';
 export default function Loading() {
   return (
     <div className="post-content-layout">
+      <div className="mb-4 md:mb-6">
+        <Skeleton className="w-1/12 h-6 rounded-xl mb-3" />
+      </div>
+      <div className="mb-6 md:mb-8 lg:mb-10">
+        <Skeleton className="w-2/3 h-10 rounded-xl mb-4 md:mb-5 lg:mb-6" />
+        <Skeleton className="w-1/4 h-4 rounded-xl mb-2 md:mb-3 lg:mb-4" />
+        <Skeleton className="w-1/12 h-5 rounded-xl mb-2 md:mb-3 lg:mb-4" />
+      </div>
+
       <div className="md:mb-8 lg:mb-10">
         <Skeleton className="w-3/5 h-8 rounded-xl mb-4 md:mb-5 lg:mb-6" />
         <Skeleton className="w-4/5 h-6 rounded-xl mb-2 md:mb-3 lg:mb-4" />
--- app/hooks/useFilteredPosts.tsx
+++ app/hooks/useFilteredPosts.tsx
@@ -1,10 +1,43 @@
+// import { useMemo } from 'react';
+// import { Post as PostType } from '@/lib/notion/types';
+
+// interface UseFilteredPostsProps {
+//   posts: PostType[];
+//   searchKeyword: string;
+//   categoryFilter?: string;
+//   tagFilter?: string;
+// }
+
+// export function useFilteredPosts({
+//   posts,
+//   searchKeyword,
+//   categoryFilter = 'all',
+//   tagFilter = 'all',
+// }: UseFilteredPostsProps) {
+//   const filteredPosts = useMemo(() => {
+//     return posts.filter((post) => {
+//       if (categoryFilter !== 'all' && post.category !== categoryFilter)
+//         return false;
+//       if (tagFilter !== 'all' && !post.tags.includes(tagFilter)) return false;
+//       if (
+//         searchKeyword &&
+//         !post.title.toLowerCase().includes(searchKeyword.toLowerCase())
+//       )
+//         return false;
+//       return true;
+//     });
+//   }, [posts, searchKeyword, categoryFilter, tagFilter]);
+
+//   return filteredPosts;
+// }
+
 import { useMemo } from 'react';
 import { Post as PostType } from '@/lib/notion/types';
 
 interface UseFilteredPostsProps {
   posts: PostType[];
   searchKeyword: string;
-  categoryFilter?: string;
+  categoryFilter?: { name: string; color: string } | string;
   tagFilter?: string;
 }
 
@@ -16,9 +49,21 @@ export function useFilteredPosts({
 }: UseFilteredPostsProps) {
   const filteredPosts = useMemo(() => {
     return posts.filter((post) => {
-      if (categoryFilter !== 'all' && post.category !== categoryFilter)
+      if (
+        categoryFilter !== 'all' &&
+        typeof categoryFilter === 'string' &&
+        post.category.name !== categoryFilter
+      )
         return false;
-      if (tagFilter !== 'all' && !post.tags.includes(tagFilter)) return false;
+      // if (
+      //   tagFilter !== 'all' &&
+      //   !post.tags.some((tag) => tag.name === tagFilter)
+      // )
+      if (
+        tagFilter !== 'all' &&
+        !post.tags.map((tag) => tag.name).includes(tagFilter)
+      )
+        return false;
       if (
         searchKeyword &&
         !post.title.toLowerCase().includes(searchKeyword.toLowerCase())
