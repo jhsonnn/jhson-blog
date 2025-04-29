a288767a725015ac7647cb9df06ba7b6ee842b8c
jhsonnn <jihyeongson13@gmail.com>
2025-04-29 14:59:43 +0900

    Chore : 주석 제거

--- lib/notion/api/fetchNotionPostsbyPage.ts
+++ lib/notion/api/fetchNotionPostsbyPage.ts
@@ -12,47 +12,6 @@ export async function fetchNotionPostsByPage(page: number, pageSize: number) {
     },
   });
 
-//   const posts = response.results.filter(isPageObjectResponse).map((post) => ({
-//     id: post.id,
-//     title:
-//       post.properties.title?.type === 'title'
-//         ? post.properties.title.title[0]?.plain_text || ''
-//         : '',
-//     slug:
-//       post.properties.slug?.type === 'rich_text'
-//         ? post.properties.slug.rich_text[0]?.plain_text || ''
-//         : '',
-//     category:
-//       post.properties.category?.type === 'select'
-//         ? post.properties.category.select?.name || ''
-//         : '',
-//     tags:
-//       post.properties.tags?.type === 'multi_select'
-//         ? post.properties.tags.multi_select.map((tag) => tag.name)
-//         : [],
-//     thumbnailUrl:
-//       post.properties.thumbnailUrl?.type === 'files' &&
-//       Array.isArray(post.properties.thumbnailUrl.files) &&
-//       post.properties.thumbnailUrl.files.length > 0
-//         ? post.properties.thumbnailUrl.files[0]?.type === 'file'
-//           ? post.properties.thumbnailUrl.files[0]?.file?.url || '/default_image.png'
-//           : post.properties.thumbnailUrl.files[0]?.type === 'external'
-//             ? post.properties.thumbnailUrl.files[0]?.external?.url || '/default_image.png'
-//             : '/default_image.png'
-//         : '/default_image.png',
-//     date:
-//       post.properties.date?.type === 'date' && post.properties.date.date?.start
-//         ? post.properties.date.date.start
-//         : '',
-//     status:
-//       post.properties.status?.type === 'status'
-//         ? { name: post.properties.status.status?.name }
-//         : { name: 'private' },
-//   }));
-
-//   return { posts };
-// }
-
 const posts = response.results.filter(isPageObjectResponse).map((post) => {
     const properties = post.properties;
 
